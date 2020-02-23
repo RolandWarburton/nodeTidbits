e3e34c5aa6890c5603e146e624c1d1f7b0d05895
RolandWarburton <warburtonroland@gmail.com>
2020-02-23 20:53:57 +1100

    :fire: Remove failed boilerplate

--- express-react-webpack-babel-setup/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './app'
-const title = 'Hello world!';
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('app')
-);

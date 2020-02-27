e2e66fa9f38e53dee8f2ed631b1802f31f9c266c
RolandWarburton <warburtonroland@gmail.com>
2020-02-27 21:09:02 +1100

    :rocket: Add Express-Webpack boilerplate

---
+++ express-static-app-boilerplate/README.md
@@ -0,0 +1,6 @@
+# Express and Webpack Boilerplate App
+
+### TODO
+- [ ] Find a way to do multipages
+- [ ] understand HMR (HotModuleReplacement)
+- [ ] Document and fully understand how this boilerplate works
---
+++ express-static-app-boilerplate/webpack.production.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  target: 'web',
+  devtool: '#source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module: {
+    rules: [
+      {
+        // Loads the javacript into html template.
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [{ loader: 'url-loader' }]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/html/index.html',
+      filename: './index.html',
+      favicon: "./src/img/favicon.ico"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+}

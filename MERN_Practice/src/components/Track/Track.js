3f23ae6eebed0a946fac000ca1b40672d677e688
RolandWarburton <warburtonroland@gmail.com>
2020-04-15 19:56:30 +1000

    :fire: Removed old code

--- MERN_Practice/src/components/Track/Track.js
+++ MERN_Practice/src/components/Track/Track.js
@@ -1,14 +1,5 @@
 import React, { Fragment, useState } from "react"
 import Meta from "../Meta"
-import axios from "axios"
-
-function getBase64(url) {
-	return axios
-		.get(url, {
-			responseType: 'arraybuffer'
-		})
-		.then(response => Buffer.from(response.data, 'binary').toString('base64'))
-}
 
 const Track = ({ match }) => {
 	const params = match.params
